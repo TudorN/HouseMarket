3978ba0d49be5aef1234555b5bdb4b62522ca221
Tudor Nedelcu <tudor.nedelcu.phone@gmail.com>
2022-08-16 14:26:43 +0200

    Added houseMarketController.

---
+++ HouseMarket/Controllers/HouseMarketOfferController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseMarket.Controllers
+{
+    public class HouseMarketOfferController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
