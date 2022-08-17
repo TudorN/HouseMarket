f4eee615ba7241476e176588609943f7460ead87
Tudor Nedelcu <tudor.nedelcu.phone@gmail.com>
2022-08-17 20:11:50 +0200

    Removed Ninject. Added table for the houses with garden.

--- HouseMarket/Controllers/HouseMarketOfferController.cs
+++ HouseMarket/Controllers/HouseMarketOfferController.cs
@@ -11,30 +11,42 @@ namespace HouseMarket.Controllers
     {
         private readonly ITopBrokerService _topBrokerService;
         private readonly IHouseMarketOfferService _houseMarketOfferService;
+        private readonly HttpClient _httpClient;
         public HouseMarketOfferController(ITopBrokerService topBrokerService, IHouseMarketOfferService houseMarketOfferService)
         {
             _topBrokerService = topBrokerService;
             _houseMarketOfferService = houseMarketOfferService;
+            _httpClient = new HttpClient();
         }
         public IActionResult Index()
         {
-            var httpClient = new HttpClient();
             var model = new BrokersModel();
             List<Broker> top10Brokers;
+            List<Broker> top10BrokersWithGarden;
 
             try
             {
-                var houseMarketOffer = _houseMarketOfferService.GetHouseOffer(httpClient, false);
+                var houseMarketOffer = _houseMarketOfferService.GetHouseOffer(_httpClient, false);
+                var houseMarketOfferWithGarden = _houseMarketOfferService.GetHouseOffer(_httpClient, true);
+                
                 top10Brokers = _topBrokerService.GetTopBrokers(houseMarketOffer, 10);
+                top10BrokersWithGarden = _topBrokerService.GetTopBrokers(houseMarketOfferWithGarden, 10);
             }
-            catch (Exception)
+            catch (Exception)// in case the api doesn't work
             {
-                var sampleDataString = System.IO.File.ReadAllText("..\\HouseMarket.OfferService\\bin\\Debug\\net6.0\\DataModel\\InputData\\Json\\Houses.json");
-                var sampleDataHouseMarket = JsonSerializer.Deserialize<HouseMarketOffer>(sampleDataString);
-                top10Brokers = _topBrokerService.GetTopBrokers(sampleDataHouseMarket, 10);
+                var sampleDataString1 = System.IO.File.ReadAllText("..\\HouseMarket.OfferService\\bin\\Debug\\net6.0\\DataModel\\InputData\\Json\\Houses.json");
+                var sampleDataObject1 = JsonSerializer.Deserialize<HouseMarketOffer>(sampleDataString1);
+
+                var sampleDataString2 = System.IO.File.ReadAllText("..\\HouseMarket.OfferService\\bin\\Debug\\net6.0\\DataModel\\InputData\\Json\\HousesWithGarden.json");
+                var sampleDataObject2 = JsonSerializer.Deserialize<HouseMarketOffer>(sampleDataString2);
+
+
+                top10Brokers = _topBrokerService.GetTopBrokers(sampleDataObject1, 10);
+                top10BrokersWithGarden = _topBrokerService.GetTopBrokers(sampleDataObject2, 10);
             }
           
             model.Top10Brokers = top10Brokers;
+            model.Top10BrokersHousesWithGarden = top10BrokersWithGarden;
           
 
             return View(model);
